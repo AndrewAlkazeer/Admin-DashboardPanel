7bdbc6805d2666e93b55899b7843258fe3a5617f
Andrew Alkazeer <ferasalkzer@hotmail.com>
2020-05-21 20:03:58 -0600

    changed to build instead of public

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(bodyParser.json());
 // changed path to public but it should be back to build
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'app/public')))
+app.use(express.static(path.join(__dirname, 'app/build')))
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/app/public/index.html'))
+  res.sendFile(path.join(__dirname + '/app/build/index.html'))
 })
 
 app.get('/login', (req, res)=>{
