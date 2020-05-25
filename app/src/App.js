1f7ae2baaa42fdff95c8df9ef3ad505222259beb
Andrew Alkazeer <ferasalkzer@hotmail.com>
2020-05-25 00:14:19 -0600

    added mongodb connection

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Wave from './Imgs/wave.svg';
 import User from './Imgs/user.svg';
-
+import Code from './Imgs/code.jpeg';
 class App extends React.Component{
 constructor(props){
   super(props);
@@ -13,14 +13,17 @@ render(){
 <React.Fragment>
 <div className="nav-cont">
 </div>
-<img src={Wave} alt="Wave img" />
+<img className="bg-img" src={Code} alt="background image" />
 <div className="login-cont">
 <img src={User} alt="Login Photo" />
 <h1>Welcome</h1>
 <form className="login-form" action="/login" method="POST">
-<input type="text" name="User" placeholder="User Name" />
-<input type="password" name="Password" placeholder="Password" />
-<button type="submit">Login</button>
+<input type="text" name="User" placeholder="User Name" required />
+<input type="password" name="Password" placeholder="Password" required />
+<button type="submit">LOGIN
+<div className="login-btn-div-1"></div>
+<div className="login-btn-div-2"></div>
+</button>
 </form>
 </div>
 </React.Fragment>
--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const env = require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const Mongo = require('mongodb').MongoClient;
+const assert = require('assert');
+const client = new Mongo(process.env.URI, { useNewUrlParser: true });
 app.use(cors());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,7 +16,7 @@ app.use(bodyParser.json());
 // changed path to public but it should be back to build
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'app/build')))
+app.use(express.static(path.join(__dirname, 'app/public')))
 // Anything that doesn't match the above, send back index.html
 /*
 app.get('*', (req, res) => {
@@ -22,15 +25,35 @@ app.get('*', (req, res) => {
   
 })
 */
+
+
 app.post('/login', (req, res)=>{
+  
+  client.connect(err =>{
+    if(err) throw err;
+    var resultArray = [];
+    const db = client.db("registeredUser");
+    var cursor = db.collection('userinfos').find();
+
+    function iterateFunc(doc) {
+     // console.log(JSON.stringify(doc, null, 4)); // I can see the data HERE! but cant be pushed to an array!
+    //  resultArray.push(doc);
+   }
    
+   function errorFunc(error) {
+      console.log(error);
+   }
+   
+   cursor.forEach(iterateFunc, errorFunc);
+    resultArray.push(cursor);
+    console.log(resultArray);
+  client.close();
+  });
   var user = req.body.User;
   var pass = req.body.Password;
-  console.log('user + pass');
-  console.log(user + pass);
+ // res.json(cursor);
   res.json({User: user, Pass: pass});
 });
-
 
 app.listen(port, ()=>{
     console.log(`Connected on port: ${port}`);
